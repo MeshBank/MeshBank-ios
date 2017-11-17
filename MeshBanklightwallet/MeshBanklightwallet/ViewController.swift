80f94bae2d88e691e5cc28ea180cc06fea5a2e19
MeshBank Team <westleymoorhead@gmail.com>
2017-11-17 14:05:54 -0800

    Add files via upload

---
+++ MeshBanklightwallet/MeshBanklightwallet/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MeshBanklightwallet
+//
+//  Created by Westley Moorhead on 11/16/17.
+//  Copyright © 2017 Westley Moorhead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
